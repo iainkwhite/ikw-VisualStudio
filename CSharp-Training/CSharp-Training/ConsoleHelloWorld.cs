348cc4f1d1bc4360d808145d09d5e320b520034c
Iain White <iainkwhite@users.noreply.github.com>
2017-06-27 00:13:13 -0400

    Initial Add of solution

---
+++ CSharp-Training/CSharp-Training/ConsoleHelloWorld.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Hello, Julie! ";
+            string b = " ";
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            string c = Console.ReadLine();
+            Console.WriteLine("You typed: " + c);
+        }
+    }
+}
