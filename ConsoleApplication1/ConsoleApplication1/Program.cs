df651d45d1ed102f9bdfc7bd25840fb422a53a4c
Iain White <iainkwhite@users.noreply.github.com>
2017-08-19 15:26:24 -0400

    Add ConsoleApplication1
    C# .NET 4.6 chapter 3 exercises

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            // Set up console UI
+            Console.Title = "Julie console";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Welcome to my console app");
+            Console.BackgroundColor = ConsoleColor.Black;
+            
+            // wait for Enter
+            Console.ReadLine();
+            MessageBox.Show("All done");
+
+
+            return 0;
+        }
+    }
+}
