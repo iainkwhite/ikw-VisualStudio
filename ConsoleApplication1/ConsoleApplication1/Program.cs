cc6adf488e296c6b095596be639fc127bf953631
Iain White <iainkwhite@users.noreply.github.com>
2017-08-22 12:15:13 -0400

    Add argument processing
    Add argument processing and Environment display

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -17,13 +17,32 @@ namespace ConsoleApplication1
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.WriteLine("Welcome to my console app");
             Console.BackgroundColor = ConsoleColor.Black;
-            
+
+            // Get arguments using System.Environment.
+            string[] theArgs = Environment.GetCommandLineArgs();
+            foreach (string arg in theArgs)
+                Console.WriteLine("Arg: {0}", arg);
+                
+            // Helper method within the Program class.
+            ShowEnvironmentDetails();
+
             // wait for Enter
             Console.ReadLine();
             MessageBox.Show("All done");
-
-
             return 0;
+        }
+        private static void ShowEnvironmentDetails()
+        {
+            // Print out the drives on this machine,
+            // and other interesting details.
+            foreach (string drive in Environment.GetLogicalDrives())
+                Console.WriteLine("Drive: {0}", drive);
+
+            Console.WriteLine("OS: {0}", Environment.OSVersion);
+            Console.WriteLine("Number of processors: {0}",
+              Environment.ProcessorCount);
+            Console.WriteLine(".NET Version: {0}",
+              Environment.Version);
         }
     }
 }
